567759426c96fc402837486a35bb42ef176a1030
Miguel2206 <92831039+MiguelDev2206@users.noreply.github.com>
2025-11-07 16:46:45 -0500

    Primera versión con Git LFS habilitado

---
+++ Build/d75844ef67bc5bd6550e1ce406141d9d.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:891c6b6896a185f962ddf9c5428ce2fc18fa4111c77ad43f635e60203353751e
+size 26454
---
+++ Build/f3c1f6c7d1a1a3b82c01577bc68c29ff.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d1afa85b59e931d478cdcb070b30fce56b8b484635f8b81b05e9413057fd374
+size 506026
